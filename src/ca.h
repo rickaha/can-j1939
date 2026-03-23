3f7cf33acbbf5775c0b0a478b428feb4b3310505
Rickard Häll <rickard@altavision.se>
2026-03-23 19:34:19 +0100

    Add new header ca.h
    Create src/ca
    - Add SPDX MIT license
    - Include pgn_data.h, stack_utils.h, and <stdint.h>
    - Add include guards.

---
+++ src/ca.h
@@ -0,0 +1,13 @@
+/*
+ * SPDX-License-Identifier: MIT
+ *
+ * Copyright (c) 2026 Rickard Häll
+ */
+#ifndef CA_H
+#define CA_H
+
+#include "pgn_data.h"
+#include "stack_utils.h"
+#include <stdint.h>
+
+#endif /* CA_H */
